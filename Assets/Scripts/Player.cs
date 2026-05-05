a1ce92953c2b817559c53849ea11882a49b7113e
yunnoxss115-droid <yunnoxss.115@gmail.com>
2026-05-05 16:47:31 -0300

    criei variavel altura

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,7 @@ public class Player : MonoBehaviour
 {
     public float idadeNat;
     public float nomeNat;
+    public float altura;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
